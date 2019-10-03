5edfb11e841921c59518cf6fe167117ea190f83f
Anushka Raj <anush6633@gmail.com>
2019-10-03 12:24:40 +0530

    Add files via upload

---
+++ Day 057 - Ikosaeder/README.markdown
@@ -0,0 +1,4 @@
+# 100 Days CSS Challenge - #57 Ikosaeder
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/Anushka_Raj/pen/ZPOGyq](https://codepen.io/Anushka_Raj/pen/ZPOGyq).
+
+ 
---
+++ Day 057 - Ikosaeder/src/script.js
@@ -0,0 +1,7 @@
+// jQuery v3.3.1 is supported
+$('.frame').click(function() {
+	$('.shadow').toggleClass('hide');
+	$('.side').toggleClass('break');
+	$('.break-me').toggleClass('hide');
+	$('.brand').toggleClass('show');
+});
